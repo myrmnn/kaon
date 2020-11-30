ec4534dff7d6dfea9d05a4acacbb177f1599e278
myrm <myrm.nn@gmail.com>
2020-11-29 20:07:37 -0500

    added animations

--- project/js/app.js
+++ project/js/app.js
@@ -1,6 +1,22 @@
-$(document).ready(function(){
-	
-	console.log("Hello, world!")
+window.addEventListener('DOMContentLoaded', () => {
+	const explore = document.querySelector('.explore');
+	const options = document.querySelector('.gov1-options');
+	const buttons = document.querySelectorAll('[data-js-page]');
+	const page = document.querySelectorAll('.page');
+	console.log(buttons);
 
+	page.forEach((pg) => {
+		pg.addEventListener('click', (e) => {
+			var goal_page = e.target.getAttribute('data-js-page');
+			console.log(goal_page);
+			pg.classList.remove('active');
+			document
+				.querySelector(".page[data-page='" + goal_page + "']")
+				.classList.add('active');
+		});
+	});
 
-})
+	explore.addEventListener('touchend', () => {
+		options.classList.toggle('show');
+	});
+});
